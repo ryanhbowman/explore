1f656d984eda4a1d1e0fbba725318fe36f22260a
Ryan Bowman <ryan@circle.co.ke>
2021-04-13 13:39:54 +0100

    working with active

--- js/main.js
+++ js/main.js
@@ -11,25 +11,51 @@ $(window).resize(function () {
   
 
 });
-
+function sentence(howmuch) {
+  $('.text').toggleClass('separated');
+  $(".blast").each(function(index){
+    $(this).removeClass('active');
+  });
+  $(".blast").each(function(index){
+    var increment = (index + 1) * howmuch
+    var topper = $(this).css("top");
+    var thing = increment + 'px';
+    
+    if(topper == '1px'){
+      $(this).css('top', thing );
+      // $(this).prepend('<span class="to-remove">what?</span>');
+    }
+    else {
+      $(this).css("top",'1px');
+      // $('.to-remove').hide().remove();
+    } 
+  });
+}
 
 $( document ).ready(function() {
   document.addEventListener("keydown", function(event) {
     if (event.key === 'e') {
-      $(".container").each(function(){
-        $(this).toggleClass('block-it');
-      });
+      sentence(40);
     }
   })
 
   $(".blastable").blast({ delimiter: "sentence", tag: "span" });
 
-  $("#party").click(function(e){
-    
+  $("#party").click(function(e){    
     e.preventDefault();
-    $(".container").each(function(){
-      $(this).toggleClass('block-it');
-    });
-  
+    sentence(40);
   });
-});
+
+  $(".blast").click(function(){
+    
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+    }
+    else {
+      $(".blast").each(function(index){
+        $(this).removeClass('active');
+      });
+      $(this).addClass('active');
+    }
+  })
+}); //end document ready
